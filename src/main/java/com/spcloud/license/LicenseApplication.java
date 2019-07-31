c34e10292e13236aa9adc285ceb7f3ea12830f00
welkin <feilongjack@126.com>
2019-07-31 10:22:52 +0800

    add eureka client

--- src/main/java/com/spcloud/license/LicenseApplication.java
+++ src/main/java/com/spcloud/license/LicenseApplication.java
@@ -3,8 +3,10 @@ package com.spcloud.license;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan(basePackages = "com.spcloud.license.mapper")
 public class LicenseApplication {
 
